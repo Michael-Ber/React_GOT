ac3b2a90472eef0b05f06f92f35673b2b1c399c1
Bershadsky Mihail Sergeevich <Michail.Bershadsky@ilimtimber.corp>
2021-07-13 16:52:27 +0800

    lesson 97 98, problems with promise

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 // import './randomChar.css';
 import styled from 'styled-components';
+import GotService from '../../services/gotService';
 
 const RandomBlock = styled.div`
     background-color: #fff;
@@ -34,28 +35,44 @@ const LiItem = styled.li`
 
 `;
 export default class RandomChar extends Component {
-
+    constructor() {
+        super();
+        // this.updateChar();
+    }
+    gotService = new GotService();
+    state = {
+        char: {}
+    };
+    onCharLoaded = (char) => {
+        this.setState({char});
+    }
+    updateChar() {
+        const id = Math.floor(Math.random()*140 + 25);
+        this.gotService.getCharacter(id)
+            .then(this.onCharLoaded)
+    }
+    
     render() {
-
+        const {char: {name, gender, born, died, culture}} = this.state;
         return (
             <RandomBlock>
-                <h4>Random Character: John</h4>
+                <h4>Random Character: {name}</h4>
                 <UlBlock>
                     <LiItem>
                         <span className="term">Gender </span>
-                        <span>male</span>
+                        <span>{gender}</span>
                     </LiItem>
                     <LiItem>
                         <span className="term">Born </span>
-                        <span>11.03.1039</span>
+                        <span>{born}</span>
                     </LiItem>
                     <LiItem>
                         <span className="term">Died </span>
-                        <span>13.09.1089</span>
+                        <span>{died}</span>
                     </LiItem>
                     <LiItem>
                         <span className="term">Culture </span>
-                        <span>Anarchy</span>
+                        <span>{culture}</span>
                     </LiItem>
                 </UlBlock>
             </RandomBlock>
--- src/services/gotService.js
+++ src/services/gotService.js
@@ -9,15 +9,40 @@ export default class GotService {
         }
         return await res.json();
     }
-    getAllCharacters() {
-        return this.getResource('/characters?page=5&pageSize=5')
+    async getAllCharacters() {
+        const res = await this.getResource(`/characters?page=5&pageSize=50`);
+        return res.map(this._transformCharacter)
     }
-    getCharacter(id) {
-        return this.getResource(`/characters/${id}`);
+    async getCharacter(id) {
+        const char = await this.getResource(`/characters/${id}`);
+        return this._transformCharacter(char);
+    }
+    async getSome(id) {
+        const ffd = await this.getResource(`/characters/${id}`);
+        return ffd;
+    }
+    getAllHouses() {
+        return this.getResource('/houses?page=5&pageSize=5');
+    }
+    getHouse(id) {
+        return this.getResource(`/houses/${id}`); 
+    }
+    getAllBooks() {
+        return this.getResource('/books?page=1&pageSize=5');
+    }
+    getBook(id) {
+        return this.getResource(`/books/${id}`);
+    }
+    _transformCharacter(char) {
+        return {
+            name: char.name,
+            gender: char.gender,
+            born: char.born,
+            died: char.died,
+            culture: char.culture
+        }
     }
 }
 
-const got = new GotService();
-
-got.getAllCharacters().then(res => console.log(res));
-got.getCharacter(34).then(res => console.log(res));
+const data = new GotService();
+console.log(data.getSome(150));
